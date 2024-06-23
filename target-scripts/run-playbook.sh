1868617e84c21ca75bcbdb852230650c05c9d9e6
devops <devops@localhost.localdomain>
2024-06-23 03:24:24 -0400

    Update new offline.yml sample

--- target-scripts/run-playbook.sh
+++ target-scripts/run-playbook.sh
@@ -12,5 +12,5 @@ cp -r ./playbook kubespray-${KUBESPRAY_VERSION}/offline-playbook
 cd kubespray-${KUBESPRAY_VERSION}
 pip install -U pip
 pip install -r requirements.txt
-ansible-playbook -i ../zubin-cluster/hosts.yaml -u devops -k -K -b --become --become-user=root offline-playbook/offline-repo.yml
-ansible-playbook -i ../zubin-cluster/hosts.yaml -u devops -k -K -b --become --become-user=root cluster.yml
+ansible-playbook -i ../cluster/hosts.yaml -u devops -k -K -b --become --become-user=root offline-playbook/offline-repo.yml
+ansible-playbook -i ../cluster/hosts.yaml -u devops -k -K -b --become --become-user=root cluster.yml
